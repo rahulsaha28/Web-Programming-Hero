d1e1a95b792d3bc5204587e12b5bfbf78075dc93
Rahul Saha <saharahul039@gmail.com>
2021-01-17 09:59:38 +0600

    Completed module 13.

---
+++ module_13/index.js
@@ -0,0 +1,26 @@
+
+
+// var firstArr = [2,4,5,6,8];
+// var secondArr = firstArr;
+
+// firstArr[firstArr.indexOf(4)] = 20;
+
+// console.log(firstArr);
+// console.log('Rahul');
+// console.log(secondArr);
+
+myFun();
+
+function myFun(){
+    console.log("LOVE U>");
+}
+
+var student = {
+    id:1,
+    phone:01951626993,
+    email:"saharahul039@gmail.com"
+}
+address = "satkhira.";
+student[address]="353 khoniker nir.";
+
+console.log(student.satkhira.);
